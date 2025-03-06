21cd187067cdc4d74186629791947d91c689a525
AiAdhP8 <aadahchour@iut.univ-paris8.fr>
2025-03-07 00:23:31 +0100

    Ajout des Exception

---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr3/jeuQuizz/utils/exceptions/FichierCorrompuException.java
@@ -0,0 +1,4 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr3.jeuQuizz.utils.exceptions;
+
+public class FichierCorrompuException extends ChargementFichierException{
+}
---
+++ src/main/java/org/univ_paris8/iut/montreuil/qdev/tp2025/gr3/jeuQuizz/utils/exceptions/PasDeFichierException.java
@@ -0,0 +1,5 @@
+package org.univ_paris8.iut.montreuil.qdev.tp2025.gr3.jeuQuizz.utils.exceptions;
+
+public class PasDeFichierException extends ChargementFichierException{
+
+}
